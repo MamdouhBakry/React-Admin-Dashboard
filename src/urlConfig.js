e85fc9d5aca4252d6810a52a08b72f0760eb4e05
mamdouh <mamdouh>
2022-01-19 23:59:15 +0200

    removing localhost and work on heroku

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://flipkart-admin-dashboard-app.herokuapp.com/";
+const baseUrl = "https://flipkart-admin-dashboard-app.herokuapp.com";
 // window.location.hostname === "localhost"
 //   ? "http://localhost:5000"
 //   : "https://flipkart-admin-dashboard-app.herokuapp.com/";

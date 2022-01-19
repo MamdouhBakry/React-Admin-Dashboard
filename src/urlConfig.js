fee4320901fe4f3a4f031e6258eb02c3677bc246
mamdouh <mamdouh>
2022-01-20 00:13:42 +0200

    removing localhost and work on heroku

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,7 @@
-const baseUrl = "https://flipkart-rest-api.herokuapp.com/api";
+const baseUrl = "https://flipkart-rest-api.herokuapp.com";
 // window.location.hostname === "localhost"
 //   ? "http://localhost:5000"
-//   : "https://flipkart-rest-api.herokuapp.com/api/";
+//   : "https://flipkart-rest-api.herokuapp.com";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
